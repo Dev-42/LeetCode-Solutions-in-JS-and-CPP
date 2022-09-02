1251d580c436fdad4ee1b01c4c2accdd23843df5
Dev Bhattacharya <71654514+Dev-42@users.noreply.github.com>
2022-09-02 09:35:36 +0530

    Time: 114 ms (62.05%), Space: 46.5 MB (97.05%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.js
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.js
@@ -1,21 +1,21 @@
-var averageOfLevels = function(root) 
+let averageOfLevels = function(root)
 {
-  let averages = [];
-  let queue = [root];
-  while (queue.length) 
-  {
-    let sum = 0;
-    const size = queue.length;
-    for (let i = 0; i < size; i++) 
-    {
-      let node = queue.shift();
-      sum += node.val;
-      if (node.left) 
-          queue.push(node.left);
-      if (node.right) 
-          queue.push(node.right);
-    }
-    averages.push(sum / size);
-  }
-  return averages;
-};
+    let average = [];
+    let queue = [root];
+    while(queue.length)
+        {
+            let sum = 0;
+            let s = queue.length;
+            for(let i = 0 ;i < s;i++)
+                {
+                    let node = queue.shift();
+                    sum += node.val;
+                    if(node.left)
+                        queue.push(node.left);
+                    if(node.right)
+                        queue.push(node.right);
+                }
+         average.push(sum/s);
+        }
+    return average;
+}
