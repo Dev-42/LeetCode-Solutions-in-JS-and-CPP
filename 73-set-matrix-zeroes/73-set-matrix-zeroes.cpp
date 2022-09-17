a9eb8cde0156b3bb99d02a2b6e96c6ea4ea6f579
Dev Bhattacharya <71654514+Dev-42@users.noreply.github.com>
2022-09-17 13:10:20 +0530

    Time: 41 ms (5.79%), Space: 13.2 MB (54.07%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,29 +2,67 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) 
     {
-        int row = matrix.size();
-        int col = matrix[0].size();
-        set<int> rows;
-        set<int> cols;
-        
-        for(int i = 0;i<row;i++)
+        int m =matrix.size();
+        int n = matrix[0].size();
+        int x =1;
+        int y =1;
+        for(int j=0;j<n;j++)
         {
-            for(int j = 0;j<col;j++)
+            if(matrix[0][j]==0)x=0;
+            
+        }
+        
+        for(int i=0;i<m;i++)
+        {
+            if(matrix[i][0]==0)y=0;
+        }
+       
+            
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
             {
-                if(matrix[i][j] == 0)
+                if(matrix[i][j]==0)
                 {
-                    rows.insert(i);
-                    cols.insert(j);
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-        for(int i = 0 ;i<row;i++)
+        
+        
+         for(int j=1;j<n;j++)
         {
-            for(int j = 0;j<col ;j++)
+           if(matrix[0][j]==0)
+           {
+               for(int i=1;i<m;i++)
+                   matrix[i][j]=0;
+           }
+        }
+        
+        for(int i=1;i<m;i++)
+        {
+            if(matrix[i][0]==0)
             {
-                if(rows.find(i) != rows.end() || cols.find(j) != cols.end())
-                    matrix[i][j] = 0;
+                for(int j=0;j<n;j++)
+                    matrix[i][j]=0;
             }
         }
+        
+        if(y==0)
+        {
+            for(int i=0;i<m;i++)
+            {
+                matrix[i][0]=0;
+            }
+        }
+        
+         if(x==0)
+        {      
+             for(int j=0;j<n;j++)
+            matrix[0][j]=0;
+        
+        }
+
     }
 };
